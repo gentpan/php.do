6051d3707b471f5b5b869afda50048dd3be8ed17
gentpan <dev@gentpan.com>
2026-07-05 04:44:09 +0500

    Align PHP theme spacing with php.net

--- header.php
+++ header.php
@@ -37,6 +37,9 @@ $search_query = isset($_GET['q']) ? clean_text($_GET['q'], 60) : '';
     <meta name="msapplication-TileColor" content="#4F5B93">
     <meta name="msapplication-TileImage" content="assets/mstile-150x150.png">
     <link rel="stylesheet" href="https://static.bluecdn.com/libs/fontawesome/7.3.0/css/all.min.css">
+    <?php if (qf_theme() === 'php') { ?>
+        <link rel="stylesheet" href="https://www.php.net/cached.php?t=1781787603&amp;f=/fonts/Fira/fira.css">
+    <?php } ?>
     <?php if (!$use_system_page_font) { foreach (qf_selected_font_urls() as $font_url) { ?>
         <link rel="stylesheet" href="<?php echo h($font_url); ?>">
     <?php } } ?>
