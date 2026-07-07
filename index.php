8b9be3d27f9a8ada62edf38e1f907abcd60cb33f
gentpan <dev@gentpan.com>
2026-07-08 02:38:36 +0500

    feat: 首页帖子 meta 时间后显示版块(类别)名称徽章

--- index.php
+++ index.php
@@ -123,6 +123,7 @@ function phpdo_render_thread_row($t) {
                 <p>
                     <a class="phpdo-author-link" href="<?php echo h(qf_url_user($t['user_id'])); ?>"><?php echo h($author); ?></a>
                     <time class="phpdo-time" datetime="<?php echo h(qf_iso8601($t['created_at'])); ?>" title="<?php echo h($t['created_at']); ?>"><?php echo h(qf_time_ago($t['created_at'])); ?></time>
+                    <?php if (!empty($t['forum_name'])) { ?><a class="phpdo-forum-tag" href="<?php echo h(qf_url_forum(intval($t['forum_id']))); ?>"><?php echo h($t['forum_name']); ?></a><?php } ?>
                 </p>
                 <div class="phpdo-thread-stats" aria-label="帖子统计">
                     <span><i class="fa-regular fa-eye" aria-hidden="true"></i><?php echo qf_format_compact_number($t['views']); ?></span>
